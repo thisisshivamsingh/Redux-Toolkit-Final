b4e2f4c5e536c5591fa116e76991ea7ac18a13c6
Shivam <thisisshivam06@gmail.com>
2022-07-23 23:36:24 +0530

    React with Redux

---
+++ react-rtk-demo/src/features/icecream/icecreamSlice.js
@@ -0,0 +1,34 @@
+// import { cakeActions } from "../cake/cakeSlice";
+import { ordered as cakeOrdered } from "../cake/cakeSlice";
+import { createSlice } from "@reduxjs/toolkit";
+// const createSlice = require("@reduxjs/toolkit").createSlice;
+
+const initialState = {
+  numOfIcecreams: 20,
+};
+
+const icecreamSlice = createSlice({
+  name: "icecream",
+  initialState,
+  reducers: {
+    ordered: (state) => {
+      state.numOfIcecreams--;
+    },
+    restocked: (state, action) => {
+      state.numOfIcecreams += action.payload;
+    },
+  },
+  //   extraReducers: {
+  //     ["cake/ordered"]: (state) => {
+  //       state.numOfIcecreams--;
+  //     },
+  //   },
+  extraReducers: (builder) => {
+    builder.addCase(cakeOrdered, (state) => {
+      state.numOfIcecreams--;
+    });
+  },
+});
+
+export default icecreamSlice.reducer;
+export const { ordered, restocked } = icecreamSlice.actions;
